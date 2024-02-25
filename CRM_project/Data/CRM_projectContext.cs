a9d4438ab01bb20afe6f058ce46b53e112d10c90
Akoriat <Deduchka0004@gmail.com>
2024-02-25 21:25:03 +0400

    Добавьте файлы проекта.

---
+++ CRM_project/Data/CRM_projectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRM_project.Models;
+
+namespace CRM_project.Data
+{
+    public class CRM_projectContext : DbContext
+    {
+        public CRM_projectContext (DbContextOptions<CRM_projectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRM_project.Models.Student> Student { get; set; } = default!;
+    }
+}
